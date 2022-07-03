23480b40d42113de2910504819e01f40d4dca918
Sean Kozen <sgkozen@gmail.com>
2022-07-03 22:46:19 +0900

    Css Normalize and Global added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// CSS style sheets
+import './styles/normalize.css';
+import './styles/global.css';
+
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
