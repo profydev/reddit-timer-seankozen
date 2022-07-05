2f51180f1bf051b01377c95056c8388d38453a5d
Sean Kozen <sgkozen@gmail.com>
2022-07-06 01:00:47 +0900

    Router fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { BrowserRouter as
-  Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Search from './components/Search';
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { render } from 'react-dom';
 import App from './App';
 
 // CSS style sheets
 import './styles/normalize.css';
 import './styles/global.css';
 
+const rootElement = document.getElementById('root');
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+render(<App />, rootElement);
