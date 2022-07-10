6952b4e1fc832f70597c23dfa528e05e889e7e73
Sean Kozen <sgkozen@gmail.com>
2022-07-10 23:52:45 +0900

    Header component added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function Header() {
+  return (
+    <>
+      <div>
+        <h1>Header Placeholder</h1>
+      </div>
+    </>
+  );
+}
+export default Header;
