018015bd3ba4d691b4381ecfaa9a36bc8336ad77
Sean Kozen <sgkozen@gmail.com>
2022-07-10 23:52:16 +0900

    Header component added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Search from './components/Search';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/search" element={<Search />} />
